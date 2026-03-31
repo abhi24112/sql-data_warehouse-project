37782c6ca8e2662f5c9a443948ca56ca2dc4c72b
abhi24112 <onlystudy24112002@gmail.com>
2026-03-31 17:10:47 +0530

    Build golden layer for data analysis

---
+++ scripts/gold/dimension validation.sql
@@ -0,0 +1,12 @@
+SELECT 
+* 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+WHERE c.customer_key IS NULL OR p.product_key IS NULL;
+
+SELECT * FROM gold.fact_sales;
+SELECT * FROM gold.dim_customers;
+SELECT * FROM gold.dim_products;
